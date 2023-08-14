23c9f5d1a52f7455184e1b9691075b85e809c6ee
Ali Mahdi <seyyedalimehdi@gmail.com>
2023-08-15 00:06:02 +0330

    Clean up game grid.

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,40 +13,37 @@ interface Props {
 const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  if (error) return <Text>{error}</Text>;
   return (
-    <>
-      {error && <Text>{error}</Text>}
-
-      <SimpleGrid
-        templateColumns={{
-          sm: "1fr",
-          md: "1fr 1fr",
-          lg: "1fr 1fr 1fr",
-          xl: "1fr 1fr 1fr 1fr",
-        }}
-        templateRows={{
-          sm: "1fr",
-          md: "1fr 1fr",
-          lg: "1fr 1fr 1fr",
-          xl: "1fr 1fr 1fr 1fr",
-        }}
-        // columns={{ sm: 1, md: 2, lg: 3, xl: 3 }}
-        spacing={5}
-        mt={5}
-      >
-        {isLoading &&
-          skeletons.map((skeleton) => (
-            <GameCardContainer key={skeleton}>
-              <GameCardSkeleton />
-            </GameCardContainer>
-          ))}
-        {data?.map((game) => (
-          <GameCardContainer key={game.id}>
-            <GameCard game={game} />
+    <SimpleGrid
+      templateColumns={{
+        sm: "1fr",
+        md: "1fr 1fr",
+        lg: "1fr 1fr 1fr",
+        xl: "1fr 1fr 1fr 1fr",
+      }}
+      templateRows={{
+        sm: "1fr",
+        md: "1fr 1fr",
+        lg: "1fr 1fr 1fr",
+        xl: "1fr 1fr 1fr 1fr",
+      }}
+      // columns={{ sm: 1, md: 2, lg: 3, xl: 3 }}
+      spacing={5}
+      mt={5}
+    >
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <GameCardContainer key={skeleton}>
+            <GameCardSkeleton />
           </GameCardContainer>
         ))}
-      </SimpleGrid>
-    </>
+      {data?.map((game) => (
+        <GameCardContainer key={game.id}>
+          <GameCard game={game} />
+        </GameCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
