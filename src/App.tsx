687935e18dbca7370911d95cf27cf405294c3643
Ali Mahdi <seyyedalimehdi@gmail.com>
2023-08-14 23:18:02 +0330

    Implement searching.

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
   sortOrder: string;
+  searchText: string;
 }
 
 function App() {
@@ -30,7 +31,11 @@ function App() {
         }}
       >
         <GridItem area="nav">
-          <Navbar />
+          <Navbar
+            onSearch={(searchText) =>
+              setGameQuery({ ...gameQuery, searchText })
+            }
+          />
         </GridItem>
 
         <Show above="lg">
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,12 +2,14 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch.tsx";
 import SearchInput from "./SearchInput.tsx";
-
-const Navbar = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+const Navbar = ({ onSearch }: Props) => {
   return (
     <HStack padding="10px ">
       <Image src={logo} boxSize="60px" />
-      <SearchInput />
+      <SearchInput onSearch={(searchText) => onSearch(searchText)} />
       <ColorModeSwitch />
     </HStack>
   );
